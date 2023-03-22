289ac85409768003bced7777e99374dddeb4bd4a
topurayhan007 <topurayhan007@gmail.com>
2023-03-22 14:28:30 +0600

    showed flags

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -2,13 +2,15 @@ import React from "react";
 
 const Country = (props) => {
   // console.log(props.country);
-  const { area, region, population, name } = props.country;
+  const { area, region, population, name, flags } = props.country;
   return (
     <div className="country">
+      <img src={flags.png} alt="" />
       <h3>Name: {name.common}</h3>
       <h5>Region: {region}</h5>
       <h5>Population: {population}</h5>
       <h5>Area: {area}</h5>
+
       {/* <h3>Name: {props.country.name.common}</h3>
       <h5>Region: {props.country.region}</h5>
       <h5>Population: {props.country.population}</h5>
