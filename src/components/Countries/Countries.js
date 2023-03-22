52122de2c52f9e9339115c6f1a0f007c8a8f7c7c
topurayhan007 <topurayhan007@gmail.com>
2023-03-22 17:42:25 +0600

    fixed react key warning

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -11,7 +11,7 @@ const Countries = () => {
   }, []);
   return (
     <div>
-      <h1>Hello from Countries.</h1>
+      <h1>Hello from Countries: {countries.length}</h1>
       <div className="countries">
         {countries.map((country) => (
           <Country
